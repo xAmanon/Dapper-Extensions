7148fa2e5473caaaee0d069e007448fd654ec256
xuanxuan.xu <xuanxuan.xu@kcex.com>
2019-03-12 18:22:21 +0800

    add expression

---
+++ DapperExtensions/Expressions/SortDirection.cs
@@ -0,0 +1,18 @@
+namespace DapperExtensions.Expressions
+{
+    /// <summary>
+    /// ÅÅÐò·½Ïò
+    /// </summary>
+    public enum SortDirection
+    {
+        /// <summary>
+        /// ÉýÐò
+        /// </summary>
+        Ascending = 0,
+
+        /// <summary>
+        /// ½µÐò
+        /// </summary>
+        Descending
+    }
+}
